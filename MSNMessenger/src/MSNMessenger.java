1d3bee4cd5745d53146d824539e63e428030ba79
Daniel Souza <danielrsouza10@gmail.com>
2023-08-01 09:23:53 -0300

    Praticando Programacao Orientada a Objetos

---
+++ MSNMessenger/src/MSNMessenger.java
@@ -0,0 +1,16 @@
+public class MSNMessenger {
+    public void enviarMensagem(){
+        System.out.println("Enviando mensagem...");
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem...");
+    }
+
+    public void validarConectadoInternet(){
+        System.out.println("Validando conexao...");
+    }
+
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando historico...");
+    }
+}
